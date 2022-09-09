be3be60b470e3e98f3f64a614e1d948350274e6f
Пащенко Владимир <pikimelposhta@gmail.com>
2022-09-09 18:07:41 +0300

    'update'

--- src/js/jobs.js
+++ src/js/jobs.js
@@ -1,6 +1,5 @@
 import "../css/common.css";
 import { Jobs } from "./modules/jobsAPI";
-import jobsTemplate from "../templates/jobs-template.hbs";
 
 const listJobsRef = document.querySelector(".js-jobs-list");
 const btnPrev = document.querySelector(".js-btn-prev");
@@ -11,28 +10,66 @@ const jobsAPI = new Jobs();
 jobsAPI.getJobs().then((data) => {
   renderJobs(data.data);
   jobsAPI.links = data.links;
-  console.log(jobsAPI.links);
+  updateStatusBtn(data.links);
 });
 
+function createMarkup(arr) {
+  return arr
+    .map(({ url, title, company_name, created_at, description, location }) => {
+      return `
+       <li class="list-item">
+        <h2><a href="${url}">${title}</a></h2>
+        <h3>${company_name}</h3>
+        <p>${new Date(created_at)}</p>
+        <hr>
+        ${description}
+        <hr>
+        ${location}
+    </li>`;
+    })
+    .join("");
+}
 function renderJobs(arr) {
-  listJobsRef.innerHTML = jobsTemplate(arr);
+  listJobsRef.innerHTML = createMarkup(arr);
 }
 
 btnPrev.addEventListener("click", onBtnPrevClick);
 btnNext.addEventListener("click", onBtnNextClick);
 
 function onBtnPrevClick() {
-  jobsAPI.page -= 1;
   jobsAPI.getPrevPage().then((data) => {
     renderJobs(data.data);
     jobsAPI.links = data.links;
+    updateStatusBtn(data.links);
   });
 }
 
 function onBtnNextClick() {
-  jobsAPI.page += 1;
   jobsAPI.getNextPage().then((data) => {
     renderJobs(data.data);
     jobsAPI.links = data.links;
+    updateStatusBtn(data.links);
   });
+}
+
+function updateStatusBtn(links) {
+  if (links.next) {
+    btnNext.disabled = false;
+  } else {
+    btnNext.disabled = true;
+  }
+
+  if (links.prev) {
+    btnPrev.disabled = false;
+  } else {
+    btnPrev.disabled = true;
+  }
+}
+
+function updateStatusBtn1(links) {
+  if (links.next) {
+    btnNext.disabled = false;
+  } else {
+    btnNext.disabled = true;
+  }
 }
