4fd832c641ef53ccc025a94798d58699449d2bd2
Пащенко Владимир <pikimelposhta@gmail.com>
2022-09-12 17:48:57 +0300

    'update'

--- src/js/books.js
+++ src/js/books.js
@@ -1,6 +1,9 @@
 import "./css/common.css";
 import cardBook from "../templates/card-books.hbs";
+import { BooksApi } from "./modules/booksAPI";
+import { Notify } from "notiflix";
 
+const newBooks = new BooksApi({ limit: 8 });
 const refs = {
   createForm: document.querySelector(".js-create-form"),
   updateForm: document.querySelector(".js-update-form"),
@@ -10,6 +13,81 @@ const refs = {
   deleteForm: document.querySelector(".js-delete-form"),
 };
 
+refs.btnLoadMore.addEventListener("click", onLoadClick);
+refs.createForm.addEventListener("submit", onCreateClick);
+refs.resetForm.addEventListener("submit", onResetClick);
+refs.updateForm.addEventListener("submit", onUpdateClick);
+refs.deleteForm.addEventListener("submit", onDeleteClick);
+
+function onDeleteClick(e) {
+  e.preventDefault();
+  const id = e.currentTarget.elements.bookId.value;
+  newBooks.deleteBook(id).then(() => {
+    Notify.failure("BOOK DELETED");
+  });
+  e.currentTarget.reset();
+}
+
+function onUpdateClick(e) {
+  e.preventDefault();
+  const { bookTitle, bookAuthor, bookDesc, bookId } = e.currentTarget.elements;
+  const book = {
+    title: bookTitle.value.trim(),
+    author: bookAuthor.value.trim(),
+    desc: bookDesc.value.trim(),
+    id: bookId.value,
+  };
+
+  newBooks.updateBook(book).then(() => {
+    Notify.warning("Book updated");
+  });
+  refs.bookList.innerHTML = "";
+  e.currentTarget.reset();
+}
+
+function onResetClick(e) {
+  e.preventDefault();
+  const { bookTitle, bookAuthor, bookDesc, bookId } = e.currentTarget.elements;
+  const book = {
+    title: bookTitle.value.trim(),
+    author: bookAuthor.value.trim(),
+    desc: bookDesc.value.trim(),
+    id: bookId.value,
+  };
+
+  newBooks.resetBook(book).then(() => {
+    Notify.info("Book reseted");
+  });
+  refs.bookList.innerHTML = "";
+  e.currentTarget.reset();
+}
+
+function onLoadClick(e) {
+  newBooks.getAllBooks().then((books) => {
+    renderBooks(books);
+    //newBooks.page += 1;
+  });
+}
+
 function renderBooks(books) {
+  //refs.bookList.insertAdjacentHTML("beforeend", cardBook(books));
   refs.bookList.innerHTML = cardBook(books);
+}
+
+function onCreateClick(e) {
+  e.preventDefault();
+  const { bookTitle, bookAuthor, bookDesc } = e.currentTarget.elements;
+  const book = {
+    title: bookTitle.value.trim(),
+    author: bookAuthor.value.trim(),
+    desc: bookDesc.value.trim(),
+  };
+  if (book.title && book.author && book.desc)
+    newBooks.createBook(book).then(() => {
+      Notify.success("Ok");
+    });
+  else {
+    Notify.failure("Error");
+  }
+  e.currentTarget.reset();
 }
--- src/js/modules/booksAPI.js
+++ src/js/modules/booksAPI.js
@@ -2,6 +2,53 @@ import { faker } from "@faker-js/faker";
 faker.seed(0);
 faker.locale = "ru";
 
+const axios = require("axios");
+
+export class BooksApi {
+  BASE_URL = "http://localhost:4040";
+
+  constructor({ page = 1, limit = 5 } = {}) {
+    this.page = page;
+    this.limit = limit;
+  }
+
+  deleteBook(id) {
+    return axios.delete(`${this.BASE_URL}/books/${id}`);
+  }
+
+  createBook(book) {
+    return axios.post(`${this.BASE_URL}/books`, book);
+  }
+
+  resetBook({ id, ...book }) {
+    return axios.put(`${this.BASE_URL}/books/${id}`, book);
+  }
+
+  updateBook({ id, ...book }) {
+    for (let key of Object.keys(book)) {
+      if (!book[key].trim()) delete book[key];
+    }
+    return axios.patch(`${this.BASE_URL}/books/${id}`, book);
+  }
+
+  getBooks() {
+    return axios
+      .get(`${this.BASE_URL}/books`, {
+        params: {
+          _page: this.page,
+          _limit: this.limit,
+        },
+      })
+      .then((response) => response.data);
+  }
+
+  getAllBooks() {
+    return axios
+      .get(`${this.BASE_URL}/books`)
+      .then((response) => response.data);
+  }
+}
+
 /* 
 static getRandomBook() {
     return {
